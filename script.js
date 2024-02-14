a42fa4e3009513452485b9b929cef3c3de6dbe88
priyanshusingh2406 <priyanshusingh8306@gmail.com>
2024-02-14 10:12:47 +0530

    third commit

--- script.js
+++ script.js
@@ -25,10 +25,10 @@ setIndicator("#ccc");
 function handleSlider(){
     inputSlider.value = passwordLength;
     lengthDisplay.innerText = passwordLength;
-    
+
     const min = inputSlider.min;
     const max = inputSlider.max;
-    inputSlider.style.backgroundSize = ((passwordLength -min)*100/(max-min)) + "% 100%";
+    inputSlider.style.backgroundSize = (( passwordLength - min ) *100/ (max - min)) + "% 100%";
 
 }
 
